59b50fd73c1ede9901b7b433ad8783a952e66ba9
Alina <alinadakhno60@gmail.com>
2023-04-12 18:59:02 +0200

    fetch tweets data from express API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { getRequest } from "./data";
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+  const [media, setMedia] = useState([]);
   useEffect(() => {
-    const data = getRequest();
-    console.log(data);
+    fetch(`http://localhost:4000/get-tweets`).then(async (response) => {
+      const body = await response.json();
+      setTweets(body.data.data);
+      setMedia(body.data.includes.media.map((med) => med.preview_image_url));
+    });
   }, []);
-
+  console.log(tweets.map((tweet) => tweet.id));
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="tweet_wrap">
+      <h1>Tweets:</h1>
+      {tweets.map((tweet) => (
+        <div>
+          <h1 className="tweet_header">ID: {tweet.id}</h1>
+          <p className="tweet_wrap">TEXT: {tweet.text}</p>
+        </div>
+      ))}
+      <div className="tweet_img_wrap">
+        <img src={media}></img>
+      </div>
     </div>
   );
 }
